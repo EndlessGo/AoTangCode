d187c606b319672eecaccf8a990795e570a190d2
EndlessGo <874699078@qq.com>
2019-05-30 23:01:36 +0800

    ATC-0001 dynamic programming

--- dynamic_programming.cpp
+++ dynamic_programming.cpp
@@ -1,4 +1,5 @@
 //53.Maximum Subarray
+//1. basic thoughts, time complexity O(n)
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -10,6 +11,23 @@ public:
             else
                 sum += num;         
             res = max(res, sum);
+        }
+        return res;
+    }
+};
+//2. DP thoughts, time complexity O(n)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int size = nums.size();
+        if (!size) return 0;
+        vector<int> dp(size, 0);
+        dp[0] = nums[0];
+        int res = dp[0];
+        for (int i = 1; i < size; ++i)
+        {
+            dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0);
+            res = max(res, dp[i]);
         }
         return res;
     }
