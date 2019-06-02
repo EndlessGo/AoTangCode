35f3b33188a337b08eca69080bb70cb7c11e8660
EndlessGo <874699078@qq.com>
2019-06-02 15:21:16 +0800

    ATC-0001 dynamic programming

--- dynamic_programming.cpp
+++ dynamic_programming.cpp
@@ -34,6 +34,7 @@ public:
 };
 
 //322. Coin Change
+//  1. DP thoughts, time complexity O(m*n) m = coins.size(), n = amount, space complexity O(n)
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -52,5 +53,39 @@ public:
             }
         }
         return dp[amount] != amount+1? dp[amount]:-1;
+    }
+};
+
+//121. Best Time to Buy and Sell Stock
+//  1. DP thoughts, time complexity O(n), space complexity O(n)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+        //profit[i] = max(profit[i-1], prices[i]-min(prices[j])), profit[i] means 1~i'th day max profit, j=0~i-1
+        int size = prices.size(), min_in_range = prices[0];
+        vector<int> profit(size, 0);
+        for (int i = 1; i < size; ++i)
+        {
+            min_in_range = min(min_in_range, prices[i-1]);
+            profit[i] = max(profit[i-1], prices[i] - min_in_range);
+        }
+        return *max_element(profit.begin(), profit.end());
+    }
+};
+//  2. DP thoughts, time complexity O(n), space complexity O(1)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int size = prices.size();
+        if (!size) return 0;
+        int min_price = prices[0];
+        int max_profit = 0;
+        for (int i = 1; i < size; ++i)
+        {
+            min_price = min(min_price, prices[i-1]);
+            max_profit = max(max_profit, prices[i] - min_price);
+        }
+        return max_profit;
     }
 };
