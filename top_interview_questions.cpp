18bcc095445e25a5f4539570fa520bec52e6200f
EndlessGo <874699078@qq.com>
2019-06-16 12:18:02 +0800

    ATC-0002 4. Median of Two Sorted Arrays

--- top_interview_questions.cpp
+++ top_interview_questions.cpp
@@ -109,7 +109,7 @@ public:
 };
 
 //4. Median of Two Sorted Arrays
-//	1. 
+//	1. Recursion, O(log(m+n))
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -119,7 +119,7 @@ public:
         int r = (m + n + 2) >> 1;
         return ( getkth(nums1, m ,nums2, n, l) + getkth(nums1, m ,nums2, n, r) ) / 2.0;
     }
-    int getkth(vector<int>& nums1, int m, vector<int>& nums2, int n, int k){
+    int getkth(vector<int>& nums1, int m, vector<int>& nums2, int n, int k){//find kth num in nums1 and nums2
         // let m <= n
         if (m > n) 
             return getkth(nums2, n, nums1, m, k);
@@ -144,6 +144,42 @@ public:
 };
 
 //	2.Binary Search, log(min(m,n)) video https://www.youtube.com/watch?v=LPFhl65R7ww
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        //ensure x <= y, to make time complexity log(min(x,y)) and we only move num in short vector
+        if (m > n) return findMedianSortedArrays(nums2, nums1);
+        //partitionX = partition(x) means divide two parts in nums1, left are[0...x-1] and right are [x...m)
+        //partitionX + partitionY = (x+y+1)/2, plus 1 ensure in odd case left part can contain one num more.
+        int low = 0, high = m;
+        while (low <= high)
+        {
+            int partitionX = (low + high) / 2;
+            int partitionY = (m + n + 1)/ 2 - partitionX;
+            
+            int maxLeftX = (partitionX == 0)?INT_MIN:nums1[partitionX-1];
+            int minRightX = (partitionX == m)?INT_MAX:nums1[partitionX];
+            
+            int maxLeftY = (partitionY == 0)?INT_MIN:nums2[partitionY-1];
+            int minRightY = (partitionY == n)?INT_MAX:nums2[partitionY];
+                
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX)
+            {
+                if ((m+n)%2)//odd
+                    return (double)max(maxLeftX, maxLeftY);
+                return (max(maxLeftX,maxLeftY) + min(minRightX, minRightY))/2.0;
+            }
+            else if (maxLeftX > minRightY)
+                high = partitionX - 1;
+            else
+                low = partitionX + 1;
+        }
+        //throw err
+        throw 0.0;
+    }
+};
 
 //41. First Missing Positive
 //	1. My thought,time complexity O(n), but space O(n)!
